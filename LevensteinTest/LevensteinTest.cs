c9a9c7f1b2d92ed92743093db64f1e514d68ee09
Александр <pyankov.work@gmail.com>
2019-09-04 08:56:08 +0300

    Add Tests

---
+++ LevensteinTest/LevensteinTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LevensteinDistanceCalculation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LevensteinTest
+{
+    [TestClass]
+    public class LevensteinTest
+    {
+        [TestMethod]
+        public void TestDistance()
+        {
+            Assert.AreEqual(0, Program.GetLevensteinDistance("тест", "тест"));
+            Assert.AreEqual(4, Program.GetLevensteinDistance("", "тест"));
+            Assert.AreEqual(2, Program.GetLevensteinDistance("тетс", "тест"));
+            Assert.AreEqual(1, Program.GetLevensteinDistance("тест", "теста"));
+            Assert.AreEqual(1, Program.GetLevensteinDistance("тест", "тес"));
+            Assert.AreEqual(1, Program.GetLevensteinDistance("тест", "тост"));
+        }
+    }
+}
---
+++ LevensteinTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LevensteinTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LevensteinTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("32eec6b5-24a2-4be9-8d1e-6d2ce350e6d0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
